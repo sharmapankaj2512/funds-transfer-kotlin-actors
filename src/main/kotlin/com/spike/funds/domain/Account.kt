e7d03ebcec5717c66928da8af4a1bdbfeb03e261
Pankaj Sharma <sharmapankaj2512@gmail.com>
2019-10-07 03:06:34 +0400

    Simplify message structure

--- src/main/kotlin/com/spike/funds/domain/Account.kt
+++ src/main/kotlin/com/spike/funds/domain/Account.kt
@@ -3,29 +3,21 @@ package com.spike.funds.domain
 import kotlinx.coroutines.CompletableDeferred
 
 class Account(balance: Int = 0) {
-    suspend fun credit(amount: Int, response: CompletableDeferred<Int> = CompletableDeferred()): CompletableDeferred<Int> {
-        actor.send(CreditAmount(amount, response))
-        return response
+    private val actor = AccountActor(balance)
+    
+    suspend fun credit(amount: Int): CompletableDeferred<Int> {
+        return actor.send(CreditAmount(amount))
     }
 
     suspend fun balance(): CompletableDeferred<Int> {
-        val response = CompletableDeferred<Int>()
-        actor.send(GetBalance(response))
-        return response
+        return actor.send(GetBalance)
     }
 
     suspend fun debit(amount: Int): CompletableDeferred<Int> {
-        val response = CompletableDeferred<Int>()
-        actor.send(DebitAmount(amount, response))
-        return response
-
+        return actor.send(DebitAmount(amount))
     }
 
     suspend fun transferTo(target: Account, amount: Int): CompletableDeferred<Int> {
-        val response = CompletableDeferred<Int>()
-        actor.send(Transfer(target, amount, response))
-        return response
+        return actor.send(Transfer(target.actor, amount))
     }
-
-    private val actor = AccountActor(balance)
 }
--- src/main/kotlin/com/spike/funds/domain/AccountActor.kt
+++ src/main/kotlin/com/spike/funds/domain/AccountActor.kt
@@ -2,43 +2,48 @@ package com.spike.funds.domain
 
 import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.channels.SendChannel
 import kotlinx.coroutines.channels.actor
 
 class AccountActor(val openingBalance: Int = 0) {
 
-    private val actor = GlobalScope.actor<AccountMessage> {
+    private val actor: SendChannel<Pair<AccountMessage, CompletableDeferred<Int>>> = GlobalScope.actor {
         var balance = openingBalance
 
-        fun debit(amount: Int) { balance -= amount }
+        fun debit(amount: Int) {
+            balance -= amount
+        }
 
-        fun credit(amount: Int) { balance += amount }
+        fun credit(amount: Int) {
+            balance += amount
+        }
 
         fun completes(response: CompletableDeferred<Int>) {
             response.complete(balance)
         }
 
-        for (msg in channel) {
-            when (msg) {
-                is CreditAmount -> {
-                    credit(msg.amount).also { completes(msg.response) }
-                }
-                is DebitAmount -> {
-                    debit(msg.amount).also { completes(msg.response) }
-                }
-                is Transfer -> {
-                    debit(msg.amount)
-                    msg.account.credit(msg.amount, msg.response)
-                }
-                is GetBalance -> completes(msg.response)
+        for ((msg, response) in channel) when (msg) {
+            is GetBalance -> completes(response)
+            is CreditAmount -> credit(msg.amount).also { completes(response) }
+            is DebitAmount -> debit(msg.amount).also { completes(response) }
+            is Transfer -> {
+                debit(msg.amount)
+                msg.target.send(CreditAmount(msg.amount), response)
             }
         }
     }
 
-    suspend fun send(message: AccountMessage) = actor.send(message)
+    suspend fun send(
+            message: AccountMessage,
+            response: CompletableDeferred<Int> = CompletableDeferred())
+            : CompletableDeferred<Int> {
+        actor.send(Pair(message, response))
+        return response
+    }
 }
 
 sealed class AccountMessage
-class CreditAmount(val amount: Int, val response: CompletableDeferred<Int>) : AccountMessage()
-class DebitAmount(val amount: Int, val response: CompletableDeferred<Int>) : AccountMessage()
-class GetBalance(val response: CompletableDeferred<Int>) : AccountMessage()
-class Transfer(val account: Account, val amount: Int, val response: CompletableDeferred<Int>) : AccountMessage()
+class CreditAmount(val amount: Int) : AccountMessage()
+class DebitAmount(val amount: Int) : AccountMessage()
+object GetBalance : AccountMessage()
+class Transfer(val target: AccountActor, val amount: Int) : AccountMessage()
